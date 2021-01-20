c51ed0184e9658a0d3ea77f634c8c60a8c1cf62d
Dmytro Shalenyi <shalenyi.dm@gmail.com>
2021-01-20 16:24:38 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+export default class App extends Component {
+  state = {};
+
+  componentDidMount() {}
+
+  componentDidUpdate(prevProps, prevState) {}
+
+  render() {
+    return (
+      <>
+        <h1>Hello, HW3</h1>
+      </>
+    );
+  }
+}
